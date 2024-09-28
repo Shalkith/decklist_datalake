98b84e3663479a7661c7f1f531832987c94bea27
Paul <pgwar2@gmail.com>
2024-09-28 10:40:50 -0700

    dbt snapshots

---
+++ dbt/mtg_datalake/snapshots/raw/moxfield/pauperedh_decks_history.sql
@@ -0,0 +1,15 @@
+{% snapshot pauperedh_decks_history %}
+{{
+    config(
+      unique_key='id',
+      strategy='check',
+      check_cols='all',
+      invalidate_hard_deletes=True,
+      target_database='mtg_datalake',
+      target_schema='mtg_datalake'
+    )
+}}
+
+select * from {{source('mtg_datalake', 'pauperedh_decks')}}
+
+{% endsnapshot %}
