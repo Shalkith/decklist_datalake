c3b0797c6979782ba6b7d0be4d58e6e884053164
Paul <pgwar2@gmail.com>
2024-09-28 18:03:16 -0700

    rebuild dbload_util

--- python/job/lib/utils/dbload_util/dbload_util.py
+++ python/job/lib/utils/dbload_util/dbload_util.py
@@ -1,8 +1,7 @@
-import sqlite3
+import sqlite3, sqlalchemy
 import logging
-import sys
+import sys,os,time,json,datetime
 import pandas as pd
-import os
 from lib.utils.parquet_util.parquet_util import ParquetUtil
 
 import logging
@@ -10,46 +9,107 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class DBLoadUtil:
-    def __init__(self,dbpath):
-        #we're just going to use sqlite3 for this prototype
-        
-        self.db_conn = sqlite3.connect(dbpath+'/'+'mtg_datalake.db')
+    def __init__(self,dbpath,dbtype='mysql',user='database_username',password='database_password',host='database_host'):
 
+        self.user = user
+        self.password = password
+        self.host = host
+        self.dbtype = dbtype
 
-    def load_data(self, table_name, list_of_files,table_nature,unix_time,index_name=False,tombstone=False):
-        #drop temp table if it exists
-        self.db_conn.execute('drop table if exists %s_mtg_datalake_temp' % table_name)
-        for filepath in list_of_files:
-            logging.info('Loading %s into the database' % filepath)
-            datafile = ParquetUtil(filepath)        
-            dataframe = datafile.read_parquet()
-            dataframe = dataframe.map(str)
-            #add a column for the unix time
-            dataframe['elts'] = unix_time
-            # load the data to a temorary table
-            dataframe.to_sql('%s_mtg_datalake_temp' % table_name, self.db_conn, if_exists='append', index=False) 
-        if table_nature == 'incremental':
-            # if the table does not exist just insert the data
-            try:
-                #create a distinct union of the two tables
-                dataframe = pd.read_sql('select distinct * from (select * from %s union select * from %s_mtg_datalake_temp)' % (table_name,table_name), self.db_conn) 
-                if tombstone:
-                    dataframe = dataframe.sort_values(by=[tombstone],ascending=True)
-                if index_name:
-                    dataframe = dataframe.drop_duplicates(subset=index_name, keep='last')
-            except Exception as e:
-                logging.error('Error loading data: %s' % e)
-                pass
-            
-            dataframe.to_sql(table_name, self.db_conn, if_exists='replace', index=False)
-        elif table_nature == 'snapshot':
-            dataframe.to_sql(table_name, self.db_conn, if_exists='replace', index=False)
+    def connect(self):
+        if self.dbtype == 'mariadb':
+            self.db_conn = sqlalchemy.create_engine(
+                'mariadb+mariadbconnector://{}:{}@{}:3306/mtg_datalake'.format(self.user, self.password, self.host)
+            )
+        elif  self.dbtype == 'mysql':
+            self.db_conn = sqlalchemy.create_engine(
+                'mysql+mysqlconnector://{}:{}@{}:3306/mtg_datalake'.format(self.user, self.password, self.host)
+            )
         else:
-            logging.error('Table nature not supported: %s' % table_nature)
-            self.db_conn.execute('drop table %s_mtg_datalake_temp' % table_name)
+            logging.error('Database type not supported: %s' % self.dbtype)
             sys.exit(1)
-        #drop the temp table
-        self.db_conn.execute('drop table %s_mtg_datalake_temp' % table_name)
+        self.db_conn = self.db_conn.connect()
+    
+    def close_connection(self):
+        self.db_conn.close()
+
+        
+    def getlastruntime(self,table_name,tombstone):
+        self.connect()
+        try:
+            last_unix_time = pd.read_sql('select max(%s) from %s' % (tombstone,table_name), self.db_conn)
+            last_unix_time = last_unix_time.iloc[0,0]
+            #convert the last run time to a unix timestamp
+            last_unix_time = int(time.mktime(datetime.datetime.strptime(str(last_unix_time), '%Y-%m-%d %H:%M:%S').timetuple()))
+        except Exception as e:
+            print(e)
+            logging.error('Error getting last run time: %s' % e)
+            last_unix_time = 0
+        #close connection
+        self.close_connection()
+        
+        return last_unix_time
+
+    def load_data(self, table_name, filepath,table_nature,end_date,index_name=False,transform=False,tombstone=False):
+        self.connect()
+        #load the data
+        debug = True
+        datafile = ParquetUtil(filepath)
+        dataframe = datafile.read_parquet()
+        if debug:
+            print('checking for transform')
+        if transform:
+            #for each key in the transform dictionary, apply the value type to the column
+            for key in transform:
+                if transform[key] == 'datetime':
+                    try:
+                        dataframe[key] = pd.to_datetime(dataframe[key])
+                    except:
+                        pass
+                if transform[key] == 'str':
+                    dataframe[key] = dataframe[key].astype(str)
+                if transform[key] == 'int':
+                    dataframe[key] = dataframe[key].astype(int)
+                if transform[key] == 'float':
+                    dataframe[key] = dataframe[key].astype(float)
+                if transform[key] == 'json':
+                    dataframe[key] = dataframe[key].apply(json.dumps)
+                if transform[key] == 'dict':
+                    dataframe[key] = dataframe[key].apply(json.dumps)
+        if debug:
+            print(dataframe.head())
+            print(index_name)
+        #if there is an index name, drop duplicates
+        if index_name:
+            if debug:
+                print('dropping duplicates')
+            dataframe = dataframe.drop_duplicates(subset=[index_name], keep='last')
+        #if nature is snapshot, truncate the table then load the data
+        
+        if table_nature == 'snapshot':
+            self.db_conn.execute('delete from %s' % table_name)
+            #load the data to the table - use long text for json columns
+            dataframe.to_sql(table_name, self.db_conn, if_exists='append', index=False, dtype={'json': sqlalchemy.types.Text})
+            print('Data loaded into table:',table_name)
+        #if nature is incremental, upsert the data
+        if table_nature == 'incremental':
+
+            if tombstone:
+                #sort the data by the tombstone column
+                dataframe = dataframe.sort_values(by=[tombstone],ascending=True)
+            #drop duplicates based on the index name
+            if index_name:
+                dataframe = dataframe.drop_duplicates(subset=index_name, keep='last')
+            #load the data to the table
+            if debug:
+                print('upserting data')
+
+            #when creating the table ensure the decklistdata column is json
+            dataframe.to_sql(table_name, self.db_conn, if_exists='append', index=False, dtype={'json': sqlalchemy.types.Text})           
+            
+            # remove duplicates from the table
+            print('Data inserted into table:',table_name)
+        self.close_connection()
 
 if __name__ == '__main__':
     # db path
