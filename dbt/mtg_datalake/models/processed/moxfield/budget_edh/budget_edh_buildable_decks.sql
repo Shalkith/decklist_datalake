fb675182c08d9cdc4089239c5e910a4d38c59032
Paul <pgwar2@gmail.com>
2025-01-16 13:45:33 -0700

    add budget decks

---
+++ dbt/mtg_datalake/models/processed/moxfield/budget_edh/budget_edh_buildable_decks.sql
@@ -0,0 +1,56 @@
+
+
+WITH decks AS (
+    SELECT
+        *
+    FROM
+        {{ ref('budget_edh_decklists') }}
+),
+mycards AS (
+    SELECT
+        NAME,
+        CASE
+            WHEN NAME LIKE 'Snow-Covered%' THEN 99
+            ELSE quantity
+        END AS quantity
+    FROM
+        {{ ref('mtga_collection') }}
+        mc
+),
+combined AS (
+    SELECT
+        d.*,
+        m.name
+    FROM
+        decks d
+        LEFT JOIN mycards m
+        ON d.card_name = m.name
+        AND d.quantity <= m.quantity
+),
+quantitycheck AS (
+    SELECT
+        DISTINCT deck_id
+    FROM
+        combined
+    WHERE
+        NAME IS NOT NULL
+    GROUP BY
+        1
+    HAVING
+        SUM(quantity) = 100
+)
+SELECT
+    *
+FROM
+    combined
+WHERE
+    deck_id IN (
+        SELECT
+            *
+        FROM
+            quantitycheck
+    )
+ORDER BY
+    1,
+    5 ASC,
+    4 DESC
---
+++ dbt/mtg_datalake/models/processed/moxfield/budget_edh/budget_edh_decklists.sql
@@ -0,0 +1,58 @@
+{{ config(
+    materialized = "view"
+) }}
+
+WITH commanders AS (
+
+    SELECT * FROM {{ ref('budget_edh_board_commanders') }}
+),
+mainboard AS (
+    SELECT * FROM {{ ref('budget_edh_board_mainboard') }}
+),
+companions AS (
+    SELECT * FROM {{ ref('budget_edh_board_companions') }}
+),
+FINAL AS (
+    SELECT
+        *,
+        'Commander' AS TYPE
+    FROM
+        commanders
+    UNION ALL
+    SELECT
+        *,
+        'Mainboard' AS TYPE
+    FROM
+        mainboard
+    UNION ALL
+    SELECT
+        *,
+        'Companion' AS TYPE
+    FROM
+        companions
+),
+quantity_validate AS (
+    SELECT
+        DISTINCT deck_id
+    FROM
+        FINAL d
+    GROUP BY
+        1
+    HAVING
+        SUM(quantity) = 100
+)
+SELECT
+    *
+FROM
+    FINAL
+WHERE
+    deck_id IN (
+        SELECT
+            *
+        FROM
+            quantity_validate
+    )
+ORDER BY
+    1,
+    5 ASC,
+    4 DESC
