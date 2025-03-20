4bfbb487a55abedda896ecb0a69bda59db1c4712
Paul <pgwar2@gmail.com>
2025-03-20 15:45:21 -0700

    rename

--- dbt/mtg_datalake/snapshots/raw/moxfield/pauperedh/pedh_decks_history.sql
+++ dbt/mtg_datalake/snapshots/raw/moxfield/pauperedh/pedh_decks_history.sql
@@ -6,7 +6,8 @@
       check_cols='all',
       invalidate_hard_deletes=True,
       target_database='mtg_datalake',
-      target_schema='mtg_datalake'
+      target_schema='mtg_datalake',
+      alias='pauperedh_decks_history'
     )
 }}
 
