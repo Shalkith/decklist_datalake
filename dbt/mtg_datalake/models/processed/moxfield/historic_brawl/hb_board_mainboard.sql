8224ddf731b96cf897096c7731d15e2d6f1cc68a
Paul <pgwar2@gmail.com>
2025-03-20 15:36:32 -0700

    historic brawl w/macros

--- dbt/mtg_datalake/models/processed/moxfield/historic_brawl/hb_board_mainboard.sql
+++ dbt/mtg_datalake/models/processed/moxfield/historic_brawl/hb_board_mainboard.sql
@@ -1,15 +1,8 @@
-with 
-decks as (
-SELECT id,
-	JSON_UNQUOTE(JSON_EXTRACT(deckdata, '$.format')) as format,
-    JSON_UNQUOTE(JSON_EXTRACT(deckdata, CONCAT('$.boards.mainboard.cards.', `key`, '.card.name'))) AS card,
-    JSON_UNQUOTE(JSON_EXTRACT(deckdata, CONCAT('$.boards.mainboard.cards.', `key`, '.quantity'))) AS quantity   
-FROM {{ref('historicbrawl_decks_history')}},
-  JSON_TABLE(
-    JSON_KEYS(JSON_EXTRACT(deckdata, '$.boards.mainboard.cards')),
-    '$[*]' COLUMNS (`key` VARCHAR(255) PATH '$')
-  ) AS t
-WHERE dbt_valid_to IS NULL 
-)
-SELECT id as deck_id,card as card_name, format,quantity
-FROM decks
+{{
+    config(
+        materialized='incremental',
+        unique_key=['deck_id','card_name']
+    )
+}}
+
+{{board_mainboard('hb')}}
